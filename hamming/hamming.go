28072d9a079fd66dddafdb0c5b6d308fef4ae74d
Anastasiya Shaternik <anastasiya.shaternik@savedo.de>
2019-11-26 13:58:46 +0100

    [hamming] Make use of range

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -5,16 +5,14 @@ import "errors"
 // Distance founds Hamming distance of two DNA codes
 func Distance(a, b string) (int, error) {
 	aRunes, bRunes := []rune(a), []rune(b)
-
-	sampleLen := len(aRunes)
 	var distance int
 
-	if sampleLen != len(bRunes) {
+	if len(aRunes) != len(bRunes) {
 		err := errors.New("DNAs should be the same length")
 		return 0, err
 	}
 
-	for i := 0; i < sampleLen; i++ {
+	for i := range aRunes {
 		if aRunes[i] != bRunes[i] {
 			distance++
 		}
